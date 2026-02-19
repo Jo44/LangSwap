f7ffccc4529b5228a52746af1435e3073be1e1c1
Jo <38272817+Jo44@users.noreply.github.com>
2026-02-19 09:06:59 +0100

    Update

--- Configuration.cs
+++ Configuration.cs
@@ -32,8 +32,8 @@ public class Configuration : IPluginConfiguration
 
     // Signatures
     public string CastBarSig { get; } = "48 83 EC 38 48 8B 92";
-    public string ItemHoveredSig { get; } = "E8 ?? ?? ?? ?? 84 C0 0F 84 ?? ?? ?? ?? 48 89 9C 24 ?? ?? ?? ?? 4C 89 A4 24";
     public string GenerateActionTooltipSig { get; } = "E8 ?? ?? ?? ?? 48 8B 43 28 48 8B AF";
+    public string ItemHoveredSig { get; } = "E8 ?? ?? ?? ?? 84 C0 0F 84 ?? ?? ?? ?? 48 89 9C 24 ?? ?? ?? ?? 4C 89 A4 24";
     public string GenerateItemTooltipSig { get; } = "48 89 5C 24 ?? 55 56 57 41 54 41 55 41 56 41 57 48 83 EC ?? 48 8B 42 ?? 4C 8B EA";
 
     // Miscellaneous
